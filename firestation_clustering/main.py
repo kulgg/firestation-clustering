591f01ce3de8bfdc9edd160e4080d971369680ff
kulgg <75735874+kulgg@users.noreply.github.com>
2022-12-27 13:19:03 +0100

    Add pipeline and vscode settings

--- firestation_clustering/main.py
+++ firestation_clustering/main.py
@@ -13,7 +13,9 @@ def configure_logging():
     log_format = "%(asctime)s [%(levelname)s] %(module)s.%(funcName)s(): %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_format)
 
+
 def main():
     config = load_config()
     configure_logging()
     logging.info("hello")
+    logging.info(config)
