364d332edfb80e1e20daa6917c2b38a0dcf369a0
kulgg <75735874+kulgg@users.noreply.github.com>
2022-12-27 13:16:51 +0100

    Add poetry project

---
+++ firestation_clustering/main.py
@@ -0,0 +1,19 @@
+import logging
+import yaml
+
+
+def load_config():
+    config = {}
+    with open("config.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
+
+
+def configure_logging():
+    log_format = "%(asctime)s [%(levelname)s] %(module)s.%(funcName)s(): %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_format)
+
+def main():
+    config = load_config()
+    configure_logging()
+    logging.info("hello")
